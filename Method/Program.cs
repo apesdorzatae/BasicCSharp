4172cbfa46d92f6d1f6bdbd5076ad242aa534631
apesdorzatae <faikha'adzrookaamilaqurrota'aina@bmd.sch.id>
2024-07-30 14:52:41 +0700

    Menamvahkan penggunaan Method

---
+++ Method/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Memanggil method");
+             hadapKanan();
+             Console.WriteLine("Dan");
+             hadapKiri();    
+
+         }
+         static void hadapKanan()
+         {
+             Console.WriteLine("Menghadap ke kanan");
+         }
+         static void hadapKiri()
+         {
+             Console.WriteLine("Menghadap ke kiri");*/
+
+            Console.WriteLine("Masukkan jumlah langkah :");
+
+            Melangkah(int.Parse(Console.ReadLine()));
+        }
+        static void Melangkah( int jumlahLangkah)
+        {
+            Console.WriteLine("Telah melangkah sebanyak {0} kali",jumlahLangkah);
+        }
+        //#Mantap
+        //#Bar-Bar dulu gak sih
+    }
+}
